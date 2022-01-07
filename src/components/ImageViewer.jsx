3ac93ea7ee986e6003dc3050e5dc20bbcaf05b6d
rajat <22002447+agentrajat@users.noreply.github.com>
2022-01-08 02:33:35 +0530

    Added Pagination for Image Gallery

--- src/components/ImageViewer.jsx
+++ src/components/ImageViewer.jsx
@@ -42,11 +42,11 @@ export default function ImageViewer({ onClose, imageDetail }) {
     <Dialog onClose={handleClose} open={open} maxWidth={'lg'} fullScreen={fullScreen}>
       {imageData && <>
         <DialogContent>
-          <Grid container alignItems='stretch' spacing={1}>
-            <Grid item lg={8} md={7} xs={12}>
-              <img style={{ maxWidth: "100%" }} src={imageData.largeImageURL} alt="pixabay-img" />
+          <Grid container spacing={1}>
+            <Grid item md lg>
+              <img style={{ maxWidth: "100%", maxHeight: "80vh" }} src={imageData.largeImageURL} alt="pixabay-img" />
               </Grid>
-            <Grid item container lg={4} md={5} xs={12} alignItems='center' direction="column">
+            <Grid item container md={4} xs={12} alignItems='center' direction="column">
               <Grid item><DialogTitle>{getNameFromUrl(imageData?.pageURL)}</DialogTitle></Grid>
               <Grid item container alignItems='center' justifyContent='flex-end'>
                 Uploaded by {imageData.user}<Avatar alt={imageData.user} src={imageData.userImageURL} style={{ margin: "4px 4px 0px 8px" }} />
@@ -57,7 +57,7 @@ export default function ImageViewer({ onClose, imageDetail }) {
                 <Tooltip title="Comments"><Chip style={{ margin: 4, padding: 4 }} avatar={<CommentIcon />} label={millify(imageData.comments)} /></Tooltip>
                 <Tooltip title="Downloads"><Chip style={{ margin: 4, padding: 4 }} avatar={<DownloadIcon />} label={millify(imageData.downloads)} /></Tooltip>
               </Grid>
-              <Grid item container direction="column" alignItems='center' justifyContent='center' md>
+              <Grid item container direction="column" alignItems='center' justifyContent='center' md style={{ marginTop: "4px" }}>
                 <Button variant='contained' href={imageData.pageURL} target="_blank">Download</Button>
                 <DialogContentText mt={1} textAlign='center'>Original Resolution ({imageData.imageWidth + " x " + imageData.imageHeight})<br />Size: {millify(imageData.imageSize, {
                   units: ["B", "KB", "MB", "GB", "TB"],
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,30 +1,78 @@
+import { Pagination, Stack } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import { SearchPixabayImages } from '../api/fetchApi';
 import ImageGallery from '../components/ImageGallery';
 import Search from '../components/Search';
 
+const defaultSearchData = {
+    page: 1,
+    query: ''
+};
+
 const Main = () => {
     const [imageData, setImageData] = useState([]);
-    const [query, setQuery] = useState('');
+
+    // TODO: Move this to local storage or cache
+    const [imageStore, setImageStore] = useState({});
+    const [searchData, setSearchData] = useState(defaultSearchData);
+    const [maxPages, setMaxPages] = useState(1);
 
     const handleSearch = (search) => {
-        setQuery(search);
+        setSearchData({
+            query: search,
+            page: 1
+        });
+    };
+
+    const handlePageChange = (event, value) => {
+        setSearchData({
+            ...searchData,
+            page: value
+        });
+    };
+
+    const invokeSearch = (q, page, per_page) => {
+        if(imageStore[q] && imageStore[q][page]){
+            console.log("Setting from local");
+            const localPage = imageStore[q][page];
+            setImageData(localPage.hits);
+            setMaxPages(Math.round(localPage.totalHits / per_page));
+        }else{
+            // Load from Server
+            console.log("Fetching from API");
+            SearchPixabayImages(q, page, per_page, (data) => {
+                console.log(data);
+                setImageData(data?.hits);
+                setMaxPages(Math.round(data?.totalHits / per_page));
+    
+                setImageStore({
+                    ...imageStore,
+                    [q]: {
+                        ...imageStore[q],
+                        [page]: data
+                    }
+                });
+            }, (error) => {
+                console.log(error);
+                alert('Service not available, please contact the developer!')
+            });
+        }
     }
 
     useEffect(() => {
-        SearchPixabayImages(query, 1, 10, (data) => {
-            console.log(data);
-            setImageData(data?.hits);
-        }, (error) => {
-            console.log(error);
-        });
-    }, [query]);
+        invokeSearch(searchData.query, searchData.page, 10);
+    }, [searchData]);
 
     return (
-        <>
+        <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+        >
             <Search handleSearch={handleSearch} />
             <ImageGallery itemData={imageData} />
-        </>
+            <Pagination color='primary' size='large' count={maxPages} page={searchData.page} onChange={handlePageChange} style={{marginTop: 16}}/>
+        </Stack>
     )
 }
 
